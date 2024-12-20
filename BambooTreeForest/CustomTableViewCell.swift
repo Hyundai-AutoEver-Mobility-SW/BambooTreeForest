c84323e22182c67586e70649274922330ab655e9
OhJeongHwan1 <wjdghks0316@naver.com>
2024-12-20 14:01:54 +0900

    feat: 댓글 보여주

--- BambooTreeForest/CustomTableViewCell.swift
+++ BambooTreeForest/CustomTableViewCell.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 
+protocol CustomTableViewCellDelegate: AnyObject {
+    func didTapButton(in cell: CustomTableViewCell)
+}
+
 class CustomTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    weak var delegate: CustomTableViewCellDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        delegate?.didTapButton(in: self)
+    }
 }
+
